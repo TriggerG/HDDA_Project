dfd01caae0a82c87b7d8077c03da3bec7dc1ecd5
Basti <sebastian.lieske@bioinfsys.uni-giessen.de>
2018-08-14 23:51:20 +0200

    modified files

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,14 +1,17 @@
+// function for barchart
 var drawChart = function() {
   nv.addGraph(function() {
-  var chart = nv.models.discreteBarChart()
-    .x(function(d) { return d.label })
-    .y(function(d) { return d.value })
-    .staggerLabels(true)
-    .tooltips(false)
-    .showValues(true)
+    var chart = nv.models.discreteBarChart()
+      .x(function(d) {
+        console.log('d:' , d); //console log after hovering over chart
+        return d.label }) //label from testdata.js as x value
+      .y(function(d) { return d.value }) //value from testdata.js as y value
+      .staggerLabels(true) //staggered x-labels
+      .showValues(true)
+      chart.tooltip.enabled(true);
 
   d3.select('#chart svg')
-    .datum(chartData)
+    .datum(chartData) //submit data to draw the chart
     .transition().duration(500)
     .call(chart)
     ;
@@ -19,9 +22,10 @@ var drawChart = function() {
 });
 };
 
+//after pressing 'Load Chart'
 $(function() {
-  $("#btnLoadChart").click(function(){
-      console.log('draw chart');
+  $("#btnLoadChart").click(function(){ //after clicking item #btnLoadChart write it to console
+      console.log('Draw Chart');
       drawChart();
   });
 });
