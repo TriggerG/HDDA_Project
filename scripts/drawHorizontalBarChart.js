8dbb97753cf54511462e8c1629807129c93d18a0
Basti <sebastian.lieske@bioinfsys.uni-giessen.de>
2018-08-27 22:29:21 +0200

    show bacteria in tooltips

--- scripts/drawHorizontalBarChart.js
+++ scripts/drawHorizontalBarChart.js
@@ -5,31 +5,30 @@ The consturctor of the multiBarHorizontalChart needed to be altered. THe margins
 
 */
 
-function filterClass(value, index, array) {
-  if (value.class === "accessory") { return value } //only values with accessory Pathways
-}
+var accessData = fullData.filter(filterClass)                   // filtering inputdata
+accessData.sort(function(a,b) { return b.count - a.count})  // in place sorting (descending)
 
-accessData = fullData.filter(filterClass) // filtering inputdata
-accessData.sort(function(a,b) { return b.count - a.count}) // sort descending
-
-graphData = [{
-  "key":"frequency of occurence",
+// transforming to the right structure in order to draw the barchart
+var graphData = [{
+  "key":"number of occurence",
   "colour":"#1f77b4",
   "values": accessData
-}] //right structure for the barChart
+}]
 
 var horiBarChart = function() {
   nv.addGraph(function() {
     var chart = nv.models.multiBarHorizontalChart()
         .x(function(d) { return d.name })
         .y(function(d) { return d.count})
-        .margin({top: 30, right: 80, bottom: 50, left: 250}) //changed margin-left and right to evade the yAxis' cut off
-        .showValues(true)
-        .showControls(false);
+        .margin({top: 30, right: 80, bottom: 50, left: 300}) //changing margin-left and right to evade the yAxis' cut off
+        .showValues(false)                                    // values left of the bars
+        .showControls(false)
+        .showLegend(false);
 
-    chart.tooltip.gravity("e") //tooltip right from curser
     chart.yAxis
-        .tickFormat(d3.format(',.2f'))
+        .tickFormat(d3.format(',d'))    //the y-axis is actually the x-axis because the chart is rotated
+        .axisLabel("Number of Bacteria Featuring the Pathway");
+
 
 
     d3.select('#horizontalBarChart svg')
@@ -37,20 +36,36 @@ var horiBarChart = function() {
         .transition().duration(500)
         .call(chart);
         // custom tooltip
-   chart.tooltip.gravity("e") //tooltip right from curser
-   chart.tooltip.contentGenerator(function (obj) {
-     var format = d3.format(",d");
-     return '<table class= "bartable"> <thead><tr><td class=x-value colspan=5><h1 class= tooltip>' + obj.data.key + '</h1></td></tr></thead>' +
-         '<tbody><tr><td class=key> Frequency of Occurence:</td><td class = "dcount">' + obj.data.count + '</td></tr>' + //dcount in styles.css
-         '<tr><td class=key>Available In Species: </td> <td class= "dspec">' + obj.data.availableInSpec + '</td></tr>' +
-         '</tbody></table>'
 
-     });
+    chart.tooltip
+        .gravity("e")                             //tooltip right from curser
+        .contentGenerator(function (obj) {
+          var format = d3.format(",d");
+          var arraySpecies = extractSpecies(obj.data.availableInSpec);
+          var txt = "";
+          arraySpecies.forEach(funcName);
+
+          function funcName (value, index, array) {
+            txt = txt + value.species + " " + value.lineage + "; "
+          }
+          console.log(txt);
+
+          return  '<table class="bartable"><thead><tr><td class=x-value colspan=3> <h1 class= tooltip>' + obj.data.key + '</h1></td></tr></thead>' +
+                  '<tbody><tr><td class=key> Frequency of Occurence:</td><td class = "tdcount">' + obj.data.count + '</td></tr>' + //dcount in styles.css
+                  '<tr><td class=key>Available In Species: </td> <td class= "tdspec">' + txt + '</td></tr>' +
+                  '</tbody></table>'
+        });
 
     nv.utils.windowResize(chart.update)
-
-    return chart;
-  });
-};
+})};
 
 horiBarChart();
+
+/// function //
+
+function filterClass(value, index, array) {                 //filtering only accessory Pathways
+  if (value.class === "accessory") { return value }
+}
+function extractSpecies (value, index, array) {
+  return value
+ };
